99f79ddc278717a392e8eff0da097c146927e756
Furkan Kıraç <fkirac@gmail.com>
2019-02-13 11:31:39 +0300

    First commit.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Syllabus.
+// Midterm 25%, Final 40%, Project 35%
+// Teaching Assistant: Baris Ozcan -> Office Hour to be decided
+// Intructor Office Hours available with reservation through email
+// Will use Clang 7.0.1 and C++17 standard. Sometimes we'll sneak into C++2a.
+// COMPILER CONFORMANCE: https://en.cppreference.com/w/cpp/compiler_support
+
+// We will mostly aim to write code for compile-time code running:
+// template, constexpr, inline, lambda
+
+// We will work on:
+//   polymorphic classes: Both dynamic and static polymorphism
+//   template meta programming
+//   inheritence vs composition
+//   and more ...
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
