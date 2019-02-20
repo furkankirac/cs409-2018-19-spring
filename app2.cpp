63fb12072e5cd1806056a59e7aa14c8dda802a8f
Furkan Kıraç <fkirac@gmail.com>
2019-02-20 10:33:35 +0300

    Lecture start.

---
+++ app2.cpp
@@ -0,0 +1,22 @@
+// variables and functions and scope
+//   primitive types
+//   local, global, static variables
+//   pure, member, static functions
+//   function return types
+
+// auto
+
+// console: std::cout, std::cin
+
+// function overloading
+// operator overloading
+
+// struct, class, ctor/dtor
+// inheritance
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
