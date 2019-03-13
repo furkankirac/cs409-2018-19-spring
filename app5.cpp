6cfe4754c57154985a97cecfe1ed871a6961034a
Furkan Kıraç <fkirac@gmail.com>
2019-03-13 10:23:17 +0300

    Lecture start.

---
+++ app5.cpp
@@ -0,0 +1,15 @@
+// Vec2d, Vec3d, templates (typename, non-type param)
+// CV qualifiers: const, volatile
+// const, mutable
+// inheritance
+// dynamic polymorphism, v-table
+
+#include <iostream>
+
+using namespace std;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
