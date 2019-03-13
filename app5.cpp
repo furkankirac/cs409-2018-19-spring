7051ef56948c1c52ab70e3d81b7b540314ce597c
Furkan Kıraç <fkirac@gmail.com>
2019-03-13 12:32:36 +0300

    First try in making Vec class run-time aware.

--- app5.cpp
+++ app5.cpp
@@ -16,6 +16,8 @@ using namespace std;
 
 template<int nDims, typename T = int>
 struct Vec {
+    int extra_data;
+
     array<T, nDims> values;
 
     auto dot_product(const Vec<nDims, T> b) const
@@ -39,37 +41,12 @@ struct Vec {
 
 };
 
-
-template<typename T1, typename T2 = T1>
-struct Pair : public tuple<T1, T2>
-{
-    Pair(const T1 val1, const T2 val2) : tuple<T1, T2>{val1, val2}
-    {
-    }
-};
-
 // ----------------------
 int main(int argc, char* argv[])
 {
-    cout << Vec<2>{10, 20}.dot_product({5, 3}) << endl;
-    cout << Vec<3>{10, 20, 1}.dot_product({5, 3, 2}) << endl;
+    cout << Vec<2>{2, 10, 20}.dot_product({2, 5, 3}) << endl;
+    cout << Vec<3>{3, 10, 20, 1}.dot_product({3, 5, 3, 2}) << endl;
     cout << "-----" << endl;
-
-    Vec<3, float>{10.3f, 20.0f, 1.2f}.print();
-
-    const auto v = Vec<3>{10, 20, 30};
-
-    auto p1 = Pair<int, float>{5, 10.0f};
-    auto p1b = Pair<int>{5, 20};
-    auto p2 = pair<int, float>{5, 10.0f};
-    p2.first = 20;
-    auto pair_value_first = get<0>(p1);
-
-    auto t1 = tuple<int, int, float, double, char>{10, 20, 0.5f, 23.5, 'f'};
-    auto first_value = get<0>(t1); // int
-    auto third_value = get<2>(t1); // float
-    auto double_value = get<double>(t1); // get the one and only double parameter's value
-
 
 
     return 0;
