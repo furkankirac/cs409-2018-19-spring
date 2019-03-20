a9be97466526d02c700cc16a1c6d2dbaae9f366c
Furkan Kıraç <fkirac@gmail.com>
2019-03-20 10:29:41 +0300

    Lecture start.

---
+++ app6.cpp
@@ -0,0 +1,31 @@
+// implement Image class: ctor, dtor, copy-ctor, move-ctor, copy-assign, move-assign
+// inline functions
+// make it a unique instance: deleted copy-ctor, deleted copy-assign?
+// smart pointers: unique_ptr, shared_ptr
+
+// Split image class into Matrix and Image
+// inheritance: abstract functions
+
+// explicit constructors
+
+// dynamic_cast
+
+// trailer of future lectures:
+// static_cast, const_cast, reinterpret_cast
+// curiously recurring template pattern (CRTP)
+// template parameters binding with runtime parameter types
+// template-template types, template auto, type constructors
+
+#include <iostream>
+//#include <memory>
+//#include <vector>
+//#include <stdint.h>
+//#include <assert.h>
+
+using namespace std;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
