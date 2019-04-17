2343b281aa814af8068c629a8dd9d066a705297a
Furkan Kıraç <fkirac@gmail.com>
2019-04-17 10:15:00 +0300

    Lecture start.

---
+++ app9.cpp
@@ -0,0 +1,26 @@
+// brace initialization, std::initializer_list
+
+// variadic parameters
+// variadic templates: printAll: old style recursion, simplified if version
+// pack expansion
+// initializer_list trick for runOnAll
+// fold expressions
+
+// A fancy clever constructor example
+
+// ADL: Argument dependent lookup
+// RVO: return value optimization
+// CRTP: curiously recurring template pattern
+
+// std::variant, std::map, std::any
+// structured bindings
+
+#include <iostream>
+
+using namespace std;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
