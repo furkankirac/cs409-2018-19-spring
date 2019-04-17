4188d74969026ed568219cf13e112703454cab99
Furkan Kıraç <fkirac@gmail.com>
2019-04-17 11:02:21 +0300

    std::initializer_list<T> and how it is placed in the core language. Core language is biased to deduce initializer_list<T> in {...} scenarios unless smt. specified.

--- app9.cpp
+++ app9.cpp
@@ -16,11 +16,38 @@
 // structured bindings
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct Foo
+{
+    Foo(int a, int b)
+    {
+        cout << "I have two integer parameters passed." << endl;
+    }
+
+    Foo(initializer_list<int> list)
+    {
+
+    }
+
+};
+
 
 // ----------------------
 int main(int argc, char* argv[])
 {
+    auto foo = Foo(10, 20);
+
+    auto v1 = vector<int>{10, 20, 30}; // a vector containing 3 integers whose values are 10, 20, 30
+    auto v2 = vector<int>{10}; // a vector containing 1 integer whose value is 10
+    auto v3 = vector<int>(10); // a vector containing 10 integers whose values are 0.
+
+    auto a = std::initializer_list<int>{1, 5, 10};
+
+    for(int a : {10, 20, 30}) // {10, 20, 30} is now deduces as initializer_list<int>
+        cout << a << endl;
+
     return 0;
 }
