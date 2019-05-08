ec2ccaa4b146c8b141d4b7a3b13b422c77718395
Furkan Kıraç <fkirac@gmail.com>
2019-05-08 10:39:48 +0300

    Lecture start.

---
+++ app13.cpp
@@ -0,0 +1,22 @@
+// typeid operator, std::type_info, std::type_index, std::any, std::map, std::unordered_map
+// template deduction guides
+// overloading lambdas
+
+// CRTP: curiously recurring template pattern, singleton pattern example
+// structured bindings
+
+#include <iostream>
+#include <variant>
+#include <unordered_map>
+#include <any>
+#include <vector>
+#include <typeinfo>
+#include <typeindex>
+
+using namespace std;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
