7492ba467b93a33c6738befd1239b0bd19bb4e75
Furkan Kıraç <fkirac@gmail.com>
2019-05-15 11:25:42 +0300

    std::thread example with function pointer.

--- app15.cpp
+++ app15.cpp
@@ -20,14 +20,31 @@ using namespace std;
 //    operator T() const { return ref; }
 //};
 
+void accumulate(const vector<int>& v, int& sum, int left, int right)
+{
+    sum = 0;
+    for(int i=left; i<right; ++i)
+    {
+        cout << i << endl;
+        sum += v[i];
+    }
+}
+
+
 // ----------------------
 int main(int argc, char* argv[])
 {
-    int k = 10, m = 20;
-    auto k_ref = ref(k);
-    auto V = vector{ref(k), ref(m)}; // using std::reference_wrapper<int> automatically
+//    int k = 10, m = 20;
+//    auto k_ref = ref(k);
+//    auto V = vector{ref(k), ref(m)}; // using std::reference_wrapper<int> automatically
 
-//    auto v = vector{1, 2, 3, 4, 5, 6};
+    auto v = vector{1, 2, 3, 4, 5, 6};
+    auto sum1 = 0, sum2 = 0;
+    auto t1 = thread{accumulate, ref(v), ref(sum1), 0, v.size()/2};
+    auto t2 = thread{accumulate, ref(v), ref(sum2), v.size()/2, v.size()};
+    t1.join();
+    t2.join();
+    cout << "sum1=" << sum1 << ", sum2=" << sum2 << ", total=" << (sum1+sum2) << endl;
 
     return 0;
 }
