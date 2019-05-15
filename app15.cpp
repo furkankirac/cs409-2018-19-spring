454033d7e7c83eb74ac0e0d945a4314a561eb811
Furkan Kıraç <fkirac@gmail.com>
2019-05-15 10:38:19 +0300

    Lecture start.

---
+++ app15.cpp
@@ -0,0 +1,18 @@
+// std::reference_wrapper
+// threads --> std::thread: func*, functor, lambda
+// tasks --> std::async: future, promise
+
+// structured bindings: pair, map examples
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+
+using namespace std;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
