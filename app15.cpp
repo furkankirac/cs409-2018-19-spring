82fdfc80cd221424cc345b758d25c33365749a53
Furkan Kıraç <fkirac@gmail.com>
2019-05-15 11:12:14 +0300

    ReferenceWrapper implementation and std::ref/std::reference_wrapper<T>

--- app15.cpp
+++ app15.cpp
@@ -11,8 +11,23 @@
 
 using namespace std;
 
+//template<typename T>
+//struct ReferenceWrapper
+//{
+//    T& ref;
+//    ReferenceWrapper(T& ref) : ref{ref} { }
+
+//    operator T() const { return ref; }
+//};
+
 // ----------------------
 int main(int argc, char* argv[])
 {
+    int k = 10, m = 20;
+    auto k_ref = ref(k);
+    auto V = vector{ref(k), ref(m)}; // using std::reference_wrapper<int> automatically
+
+//    auto v = vector{1, 2, 3, 4, 5, 6};
+
     return 0;
 }
