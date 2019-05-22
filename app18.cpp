42c7833f270c4db10368645c2ff14220080aa1c3
Furkan Kıraç <fkirac@gmail.com>
2019-05-22 14:02:32 +0300

    SFINAE, = delete, and static_assert

--- app18.cpp
+++ app18.cpp
@@ -9,15 +9,29 @@ using namespace std;
 template<typename...>
 struct TypeDisplayer;
 
-template<bool B, typename T>    struct EnableIf             { };
-template<typename K>            struct EnableIf<true, K>    { using type = K; };
+template<bool B, typename T = void>     struct EnableIf             { };
+template<typename K>                    struct EnableIf<true, K>    { using type = K; };
+
+template<bool B, typename T = void>
+using EnableIf_t = typename EnableIf<B, T>::type;
 
 
-template<typename T, typename K = typename EnableIf< !is_same_v<T, int>, unsigned int >::type >
+// template<typename T, typename K = typename EnableIf< !is_same_v<T, int>, unsigned int >::type >
+// template<typename T, typename K = EnableIf_t< !is_same_v<T, int>, unsigned int > >
+//template<typename T, typename = EnableIf_t< !is_same_v<T, int> > >
+//template<typename T, typename = enable_if_t< !is_same_v<T, int> > >
+//void print(const T& value, enable_if_t< !is_same_v<T, int> >* = nullptr)
+
+
+template<typename T>
 void print(const T& value)
 {
+    static_assert(!is_same_v<T, float>, "I don't like floats!");
+
     cout << value << endl;
 }
+
+//template<> void  print(const int&) = delete;
 
 // ----------------------
 int main(int argc, char* argv[])
@@ -25,7 +39,7 @@ int main(int argc, char* argv[])
 //    using AAA = EnableIf<false, int>::type;
 //    auto t = TypeDisplayer<AAA>{};
 
-    print(10);
+//    print(10);
     print(string{"hello"});
     print(3.1415f);
 
