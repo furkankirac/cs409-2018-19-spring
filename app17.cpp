bdd7d1835d09417864c62f0e78e9b19c766cfc5d
Furkan Kıraç <fkirac@gmail.com>
2019-05-22 10:44:41 +0300

    Lecture start.

---
+++ app17.cpp
@@ -0,0 +1,19 @@
+// reference collapsing rules
+// auto&&, template<typename T> T&& : perfect forwarding
+
+// std::is_enum, std::is_integral
+// SFINAE: Substitution Failure Is Not An Error
+// std::enable_if
+
+#include <iostream>
+
+using namespace std;
+
+template<typename...>
+struct TypeDisplayer;
+
+// ----------------------
+int main(int argc, char* argv[])
+{
+    return 0;
+}
